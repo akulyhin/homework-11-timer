69d66d3e5b8e26cff488ec56fa04995a9d4aee29
Artem Kulyhin <akulyhin@gmail.com>
2020-10-16 16:55:33 +0300

    Last updates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './styles.css';
+
+const refs = {
+    days: document.querySelector('[data-value="days"]'),
+    hours: document.querySelector('[data-value="hours"]'),
+    mins: document.querySelector('[data-value="mins"]'),
+    secs: document.querySelector('[data-value="secs"]')
+}
+
+const dateNow = new Date();
+
+class CountdownTimer {
+    dateNow
+}
+
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Dec 31, 2020'),
+  });
